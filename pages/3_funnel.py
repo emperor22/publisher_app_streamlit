788a67d0cbc9c8e80c867fcd359c3cb554b774c9
Algiffary Riony <algiffary@yventures.com.sg>
2025-02-11 18:58:04 +0700

    added isbn and title filter on funnel page

--- pages/3_funnel.py
+++ pages/3_funnel.py
@@ -6,17 +6,22 @@ from funnel_func import funnel_viz, compare_funnel, bar_line_cluster, df_prepro,
 
 file_path = 'data/amz_biz_rpt_dec_24.parquet'
 
-df = df_prepro(file_path)
+df = df_prepro(file_path, 'data/metadata_test.parquet')
 
 st.title('Sales & Traffic Funnel V1')
 
-options = df.parent_asin.unique().tolist()
-selected_option = st.selectbox('Select ASIN:', options, key='funnel')
+filter_mode = st.radio('Select filter mode', ['ISBN13', 'Title', 'ASIN'])
 
-date_range = df[df.parent_asin == selected_option]['date'].sort_values(ascending=False).dt.strftime('%B-%Y').unique().tolist()
+filter_col_dct = {'ASIN': 'parent_asin', 'ISBN13': 'isbn13', 'Title': 'title_x'}
+filter_col = filter_col_dct[filter_mode]
+
+options = df[filter_col].unique().tolist()
+selected_option = st.selectbox(f'Select {filter_mode}:', options, key='funnel')
+
+date_range = df[df[filter_col] == selected_option]['date'].sort_values(ascending=False).dt.strftime('%B-%Y').unique().tolist()
 date_selected = st.selectbox('Select Date Range:', date_range)
 
-df_selected = df[(df.parent_asin==selected_option) & (df.month_year == date_selected)]
+df_selected = df[(df[filter_col]==selected_option) & (df.month_year == date_selected)]
 df_funnel = funnel_data(df_selected)
 
 st.write(funnel_viz(df_funnel))
@@ -43,13 +48,13 @@ if agree:
 
     with col1:
         container = st.container(border=True)
-        options_col1 = df.parent_asin.unique().tolist()
-        comp_selected_option1 = container.selectbox('Select ASIN to Compare:', options_col1, key='asin1')
+        options_col1 = df[filter_col].unique().tolist()
+        comp_selected_option1 = container.selectbox(f'Select {filter_mode} to Compare:', options_col1, key='asin1')
         
-        date_range = df[df.parent_asin == comp_selected_option1]['date'].dt.strftime('%B-%Y').unique().tolist()
+        date_range = df[df[filter_col] == comp_selected_option1]['date'].dt.strftime('%B-%Y').unique().tolist()
         comp_date_selected1 = container.selectbox('Select Date Range:', date_range, key='date1')
         
-        df_selected_col1 = df[(df.parent_asin==comp_selected_option1) & (df.month_year == comp_date_selected1)]
+        df_selected_col1 = df[(df[filter_col]==comp_selected_option1) & (df.month_year == comp_date_selected1)]
         df_funnel_comp1 = funnel_data(df_selected_col1)
 
         avg, conversion = st.columns(2)
@@ -62,13 +67,13 @@ if agree:
 
     with col2:
         container = st.container(border=True)
-        options_col2 = df.parent_asin.unique().tolist()
-        comp_selected_option2 = container.selectbox('Select ASIN to Compare:', options_col2, key='asin2')
+        options_col2 = df[filter_col].unique().tolist()
+        comp_selected_option2 = container.selectbox(f'Select {filter_mode} to Compare:', options_col2, key='asin2')
         
-        date_range = df[df.parent_asin == comp_selected_option2]['date'].sort_values(ascending=False).dt.strftime('%B-%Y').unique().tolist()
+        date_range = df[df[filter_col] == comp_selected_option2]['date'].sort_values(ascending=False).dt.strftime('%B-%Y').unique().tolist()
         comp_date_selected2 = container.selectbox('Select Date Range:', date_range, key='date2')
         
-        df_selected_col2 = df[(df.parent_asin==comp_selected_option2) & (df.month_year == comp_date_selected2)]
+        df_selected_col2 = df[(df[filter_col]==comp_selected_option2) & (df.month_year == comp_date_selected2)]
         
         avg, conversion = st.columns(2)
         with avg:
@@ -93,8 +98,8 @@ if agree:
 
 st.title('Sales Conversion Trend V1')
 
-trend_asin = st.selectbox('Select ASIN:', options, key='trend2')
-vis_df = df[df.parent_asin == trend_asin]
+trend_asin = st.selectbox(f'Select {filter_mode}:', options, key='trend2')
+vis_df = df[df[filter_col] == trend_asin]
 date_range = vis_df.month_year.unique().tolist()
 st.write('Select Date Range')
 
